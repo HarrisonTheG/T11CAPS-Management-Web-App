5bbe55b06e7b5f748cb4a8fcc9d8d4c3098dd740
ZavierLim <75073384+ZavierLim@users.noreply.github.com>
2021-06-25 15:11:40 +0800

    i added @Bean to main application

--- T11_CAPS/src/main/java/sg/edu/iss/caps/T11CapsApplication.java
+++ T11_CAPS/src/main/java/sg/edu/iss/caps/T11CapsApplication.java
@@ -1,7 +1,9 @@
 package sg.edu.iss.caps;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class T11CapsApplication {
@@ -10,4 +12,13 @@ public class T11CapsApplication {
 		SpringApplication.run(T11CapsApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args->{
+			
+			
+			
+			
+		};
+	}
 }
